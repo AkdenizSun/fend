58f654d9a22413ae25e726a485d706f2d6855258
Yana <syanaa9@gmail.com>
2024-07-29 20:37:44 +1000

    implemented nav and section highlight

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -22,6 +22,37 @@
  * Define Global Variables
  * 
 */
+const sections = document.getElementsByTagName("section");
+let numb = sections.length;
+console.log(sections);
+
+const navbarList = document.querySelector("#navbar__list");
+for (const section of sections) {
+    const para = document.createElement("li");
+    para.innerHTML = section.getAttribute('data-nav');
+    para.classList.add('menu__link');
+    navbarList.appendChild(para);
+
+    
+  }
+
+function AddActiveClass(sections){
+  for(const section of sections){
+        let distanceFromTop = section.getBoundingClientRect().top;
+        if(distanceFromTop >= 0 && distanceFromTop <= window.innerHeight){
+            section.classList.add("your-active-class");
+        } else {
+            section.classList.remove("your-active-class")
+        }
+
+    }
+}
+
+document.addEventListener('scroll', () =>{
+    AddActiveClass(sections);
+});
+
+
 
 
 /**
@@ -59,4 +90,11 @@
 
 // Set sections as active
 
+
+// for (const section of sections) {
+//   const a = document.createElement("a");
+//   a.innerText = section.dataset.linkText;
+//   a.href = "#"+section.id;
+//   nav.appendChild(a);
+// }
 
