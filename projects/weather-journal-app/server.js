13126c7d6778932b30dcef6f4b91b0a893dd270a
Yana <syanaa9@gmail.com>
2024-08-14 22:16:58 +1000

    finishing development

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,52 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = { entries: [] };
 
 // Require Express to run server and routes
+const express = require('express');
+const app = express();
+app.get("/alldata", (request, response) => {
+    response.send(projectData);
+
+});
 
 // Start up an instance of app
-
+const bodyParser = require('body-parser');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
-
+const port = 8000;
 
 // Setup Server
+const server = app.listen(port, listening);
+function listening() {
+    console.log("server running");
+    console.log(`runnig on localhost:${port}`)
+};
+
+app.post('/add', function (request, response) {
+    console.log(request);
+    makeData(request);
+    response.send({
+        message: 'New entry was added',
+    });
+
+});
+
+
+
+function makeData(request) {
+    let newData = request.body;
+    let newEntry = {
+        date: newData.date,
+        temperature: newData.temperature,
+        content: newData.content
+    };
+    projectData.entries.push(newEntry);
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,105 @@
 /* Global Variables */
+const zipcodeAPI = 'http://api.openweathermap.org/geo/1.0/zip?zip=';
+const countryCode = 'AU';
+const apiKey = '&appid=4e9a55ab66c338b482ac68e6bde5f838&units=imperial';
 
+const tempAPI = 'https://api.openweathermap.org/data/2.5/weather?';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+async function zipcodeToLatLon(zipcode) {
+
+    let url = zipcodeAPI + zipcode + ',' + countryCode + apiKey;
+    const request = await fetch(url);
+    try {
+        const allData = await request.json();
+        return allData;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+
+}
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    let zipcode = document.getElementById('zip').value;
+    zipcodeToLatLon(zipcode)
+        //.then(x => dump(x))
+        .then(x => getTemp(x))
+        //.then(x => dump(x))
+        .then(x => post(x))
+        //.then(x => dump(x))
+        .then(() => loadData())
+        //.then(x => dump(x))
+        .then(x => updateUI(x))
+        .catch(x => dump(x));
+}
+
+async function getTemp(latLonJson) {
+    let url = tempAPI + 'lat=' + latLonJson.lat + '&lon=' + latLonJson.lon + apiKey;
+    const request = await fetch(url);
+    try {
+        const allData = await request.json();
+        return allData.main.temp;
+    } catch (error) {
+        console.log("error", error);
+    }
+
+}
+
+const postData = async (url = '', data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+function post(temp) {
+    let content = document.getElementById('feelings').value;
+
+    postData('/add', { date: newDate, temperature: temp, content: content })
+}
+
+function dump(x) {
+    console.log(x);
+    return x;
+}
+
+async function loadData() {
+
+    const response = await fetch('/alldata', { method: 'GET', });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+const updateUI = async (allData) => {
+    try {
+        lastEntry = allData.entries[allData.entries.length - 1];
+        document.getElementById('date').innerHTML = lastEntry.date;
+        document.getElementById('temp').innerHTML = lastEntry.temperature;
+        document.getElementById('content').innerHTML = lastEntry.content;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
